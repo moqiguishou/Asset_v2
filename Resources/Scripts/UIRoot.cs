4e4d6fddd38918090239d6e3359039646cb5feab
Azu <865567607@qq.com>
2017-11-13 16:03:08 +0800

    fairygui

---
+++ Resources/Scripts/UIRoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+
+public class UIRoot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UIPackage.AddPackage ("UI/TankFight");
+		GComponent comp = UIPackage.CreateObject ("TankFight", "main").asCom;
+		GRoot.inst.AddChild (comp);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
